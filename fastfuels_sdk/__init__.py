3a0f3766c2451260fdfda2eb178cebbf9e50ce31
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2025-01-22 11:12:47 -0700

    Edit imports

--- fastfuels_sdk/__init__.py
+++ fastfuels_sdk/__init__.py
@@ -1,6 +1,7 @@
 from fastfuels_sdk.domains import Domain, list_domains
 from fastfuels_sdk.inventories import Inventories, TreeInventory
 from fastfuels_sdk.grids import Grids, SurfaceGrid, SurfaceGridBuilder, TreeGrid
+from fastfuels_sdk.features import Features
 
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "SurfaceGrid",
     "SurfaceGridBuilder",
     "TreeGrid",
+    "Features"
 ]
